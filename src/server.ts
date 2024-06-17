76ea192caf7748cd4cb49a3e70e817cc34d91e53
Сергей Абрашкин <abras@MacBook-Pro-Sergej.local>
2024-06-18 00:16:39 +0300

    test router & controller

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import cors from 'cors'
 import express from 'express'
 import 'dotenv/config'
+import { router } from '@/router'
 
 const port = process.env.PORT
 
 const app = express()
 
 app.use(cors())
-
-app.get('/', (req, res) => res.send('Server is running!'))
+app.get('/', (_req, res) => res.send('Server is running!'))
+app.use('/api', router)
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`)
