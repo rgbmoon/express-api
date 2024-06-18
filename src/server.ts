0357a9f7910eba08f036a87e80ad07888207d9e5
Сергей Абрашкин <abras@MacBook-Pro-Sergej.local>
2024-06-18 13:23:58 +0300

    sequelize installed

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import 'dotenv/config'
 import { router } from '@/router'
 
-const port = process.env.PORT
+import { Sequelize } from 'sequelize'
 
 const app = express()
 
@@ -11,6 +11,15 @@ app.use(cors())
 app.get('/', (_req, res) => res.send('Server is running!'))
 app.use('/api', router)
 
-app.listen(port, () => {
-  console.log(`App listening on port: ${port}`)
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  logging: console.log,
+})
+
+app.listen(process.env.APP_PORT, async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
 })
