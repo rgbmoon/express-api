24d0054c13b9b2c1409df337ce3ecc9bc3d861b3
Сергей Абрашкин <abras@MacBook-Pro-Sergej.local>
2024-06-21 09:57:25 +0300

    vercel deploy fix try

---
+++ api/index.ts
@@ -0,0 +1,3 @@
+import { serverStart } from '../src'
+
+serverStart()
--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,26 @@ import 'dotenv/config'
 import { router } from './router'
 import { Sequelize } from 'sequelize'
 
-// ORM setup
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  logging: console.log,
-})
+export const serverStart = () => {
+  // ORM setup
+  const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+    logging: console.log,
+  })
 
-// App setup
-const app = express()
+  // App setup
+  const app = express()
 
-app.use(cors()) //TODO: configure
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/api', router)
+  app.use(cors()) //TODO: configure
+  app.use(express.json())
+  app.use(express.static('public'))
+  app.use('/api', router)
 
-app.listen(process.env.APP_PORT, async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-})
+  app.listen(process.env.APP_PORT, async () => {
+    try {
+      await sequelize.authenticate()
+      console.log('Connection has been established successfully.')
+    } catch (error) {
+      console.error('Unable to connect to the database:', error)
+    }
+  })
+}
