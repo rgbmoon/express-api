1e36ce8b5efa03454003b01e5a07911a29b3ae6c
Сергей Абрашкин <abras@MacBook-Pro-Sergej.local>
2024-06-21 11:32:13 +0300

    envs fix

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 import { DataTypes, ModelDefined, Optional, Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!)
+const sequelize = new Sequelize(process.env.POSTGRES_URL!)
 
 export interface UserAttributes {
   id: number
